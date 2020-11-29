92ed35898605b3e1182ff989186f48264cbba1af
Brian <bdnorris@gmail.com>
2020-11-29 11:30:33 -0600

    finish tips input

--- src/routes/tips.svelte
+++ src/routes/tips.svelte
@@ -1,9 +1,17 @@
 <script>
 	import Menu from '../components/Menu.svelte';
 	import DosAndDonts from '../markdown/DosAndDonts.svx';
+	import TypographyTips from '../markdown/TypographyTips.svx';
+	import Layout from '../markdown/Layout.svx';
+	import MaxWidths from '../markdown/MaxWidths.svx';
+	import Images from '../markdown/Images.svx';
 
 	const menu = [
-		['concepts', 'dos-and-donts', 'Dos and Don’ts'],
+		['tips', 'dos-and-donts', 'Dos and Don’ts'],
+		['tips', 'typography', 'Typography'],
+		['tips', 'layout', 'Layout'],
+		['tips', 'max-widths', 'Max-widths, Alignment, and Outer Margins'],
+		['tips', 'images', 'Advanced Strategies for Images'],
 	]
 </script>
 
@@ -14,6 +22,15 @@
 <article>
 	<h1>Digital Design Tips</h1>
 	<DosAndDonts />
+	<div class="divider"></div>
+	<TypographyTips />
+	<div class="divider"></div>
+	<Layout />
+	<div class="divider"></div>
+	<MaxWidths />
+	<div class="divider"></div>
+	<Images />
+	<div class="divider"></div>
 </article>
 <aside>
 	<Menu items={ menu } />
